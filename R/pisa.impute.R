13f6864f13cd3834006122e1ae26cc86478c326a
Jason Bryer <jbryer@albany.edu>
2013-02-15 20:29:44 -0500

    Added imputeMissing function.

---
+++ R/pisa.impute.R
@@ -0,0 +1,28 @@
+#' Impute missing values in PISA.
+#' 
+#' This function will use the \code{MICE} package to impute missing values separately
+#' for each group.
+#'
+#' @param toimpute the variables to impute.
+#' @param the grouping variable.
+#' @param maxToImpute the maximum percentage to attempt to impute. Must be a value
+#'        between 0 and 1.
+#' @param ... other parameters passed to \code{mice}
+#' @import mice
+impute.pisa <- function(toimpute, grouping, maxToImpute=.5, ...) {
+	cols = names(toimpute)	
+	imputePlyr <- function(x) {
+		missing = apply(x, 2, function(c) sum(is.na(c))) / nrow(x)
+		noImpute = names(missing)[which(missing > maxToImpute)]
+		x = x[,!names(x) %in% noImpute]
+		m.out = mice(x, m=1, ...)
+		complete = complete(m.out, m.out$m)
+		if(length(noImpute) > 0) {
+			complete[,noImpute] = NA
+		}
+		return(list(m.out, complete[,cols]))
+	}
+	
+	results = dlply(toimpute, .(grouping), imputePlyr, .progress='text', .parallel=FALSE)
+	return(results)
+}
